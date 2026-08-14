376bcf874c327d8bd812a43e3428b3b3811d181d
0xB0 <applesuser420@gmail.com>
2026-08-13 22:56:47 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,8 @@
 (function() {
     'use strict';
 
-    const HASH_USER = '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918';
-    const HASH_PASS = '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918';
+    const HASH_USER = 'b0d2a7c7d3d6f4a8c9e0f1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2';
+    const HASH_PASS = 'a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2';
 
     async function hashString(str) {
         const encoder = new TextEncoder();
